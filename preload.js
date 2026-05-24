19057e7f097d532860956cdd0db79ede5f24a4b8
yuanli78 <3231209841@qq.com>
2026-05-25 03:19:45 +0800

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge } = require('electron');
-
-// Expose any APIs to the renderer process if needed in the future
-contextBridge.exposeInMainWorld('electronAPI', {
-  platform: process.platform
-});
